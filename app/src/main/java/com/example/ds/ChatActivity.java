cb779a3da07ab65ca24452b9102791523160e60b
Sloth <eliaskarageorgas@protonmail.com>
2022-06-26 12:01:00 +0300

    + gallery implementation in comments

--- app/src/main/java/com/example/ds/ChatActivity.java
+++ app/src/main/java/com/example/ds/ChatActivity.java
@@ -1,11 +1,15 @@
 package com.example.ds;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 
@@ -34,6 +38,7 @@ public class ChatActivity extends AppCompatActivity {
 
         EditText messageBox = findViewById(R.id.message_box);
 
+        // Send button
         ImageButton sendButton = findViewById(R.id.send_button);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.O)
@@ -50,7 +55,28 @@ public class ChatActivity extends AppCompatActivity {
         listView = findViewById(R.id.messages_view);
         messageAdapter = new MessageAdapter(this, messageList);
         listView.setAdapter(messageAdapter);
+
+        ImageButton galleryButton = findViewById(R.id.gallery_button);
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                Log.d("ChatActivity", "GalleryButton");
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                //startActivityForResult(intent, 3);
+            }
+        });
     }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (resultCode == RESULT_OK && data != null) {
+//            Uri selectedImage = data.getData();
+//            ImageView imageView = findViewById(R.id.imageView);
+//            imageView.setImageURI(selectedImage);
+//        }
+//    }
 
     @Override
     public void onBackPressed() {
